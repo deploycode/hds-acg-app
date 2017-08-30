63f1f4d40c133b34cb596d2e68d5d7656355db4a
deploycode <gabriel@deploycode.co>
2017-08-30 14:58:30 -0500

    Generando CRUD

---
+++ src/AppBundle/Form/PostType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PostType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('title')->add('slug')->add('image')->add('content')->add('isActive')->add('isFavorite')->add('createdAt')->add('updatedAt')->add('user')->add('menu');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Post'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_post';
+    }
+
+
+}
