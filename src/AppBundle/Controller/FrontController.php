790e703dbac6ac3884b30d7b47792ef64a7bd5a5
deploycode <gabriel@deploycode.co>
2017-09-01 11:56:36 -0500

    Layout post

--- src/AppBundle/Controller/FrontController.php
+++ src/AppBundle/Controller/FrontController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Post;
 
 class FrontController extends Controller
 {
@@ -17,4 +18,18 @@ class FrontController extends Controller
 
      return $this->render('home.html.twig', array ('menus'=>$menus));
    }
+   /**
+  *@Route("/articulos/{slug}", name="articulo")
+  */
+  public function articuloAction(Post $post , $slug)
+  {
+    $em = $this->getDoctrine()->getManager();
+    $query = $em->getRepository('AppBundle:Post')->createQueryBuilder('p')
+    ->orderBy('p.updatedAt', 'DESC')
+    ->getQuery();
+    $menus = $em->getRepository('AppBundle:Menu')->findAll();
+    $post= $em->getRepository('AppBundle:Post')->findOneBySlug($slug);
+    $collage = $query->getResult();
+    return $this->render('post.html.twig' , array('post' => $post , 'collage'=>$collage , 'menus'=>$menus) );
+  }
 }
